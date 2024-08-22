9873aa306b85b021852d8a00b10a72cab878d367
Antonio Tadeu Maffeis <tadeu.maffeis@gmail.com>
2024-08-22 15:36:26 -0300

    Implementação Pilha

---
+++ EDListaLinearSimpleseDupla/src/edpilha/EDStack.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edpilha;
+
+/**
+ *
+ * @author tadeu.maffeis
+ */
+public class EDStack {
+
+    private ListaLinearDupla stack;
+
+    public EDStack() {
+        this.stack = new ListaLinearDupla();
+    }
+
+    public void push(Node node) {
+        stack.append(node);
+    }
+
+    public Node pop() {
+        return stack.pop();
+    }
+
+    public void show() {
+        stack.show();
+    }
+    
+    public boolean empty()
+    {
+        return stack.empty();
+    }
+}
--- EDListaLinearSimpleseDupla/src/main/Main.java
+++ EDListaLinearSimpleseDupla/src/main/Main.java
@@ -4,116 +4,74 @@
  */
 package main;
 
-import edlld.ListaLinearDupla;
-import edlls.ListaLinearSimples;
-import edlld.Node;
+import edpilha.EDStack;
+import edpilha.Node;
 import java.util.Scanner;
 
-/**
- *
- * @author tadeu.maffeis
- */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        EDStack stackSquareBrackets = new EDStack(); // []
+        EDStack stackParentheses = new EDStack(); // ()
+        EDStack stackCurlyBrackets = new EDStack(); // {}
 
-        ListaLinearDupla list = new ListaLinearDupla();
         Scanner in = new Scanner(System.in);
-        int id = 0;
-        String sid = "";
-        String name = "";
-        String email = "";
 
-        for (; sid.compareTo("-1") != 0;) {
-            System.out.print("informations: ");
-            sid = in.next();
-            name = in.next();
-            email = in.next();
+        System.out.println("\nInforme a expressão: ");
+        String sExp = in.next();
+        boolean error = false;
 
-            if (sid.compareTo("-1") == 0) {
+        for (int i = 0; i < sExp.length(); i++) {
+            switch (sExp.charAt(i)) {
+                case '[': {
+                    stackSquareBrackets.push(new Node("1", 1));
+                    break;
+                }
+                case ']': {
+                    if (stackSquareBrackets.empty()) {
+                        error = true;
+                    } else {
+                        stackSquareBrackets.pop();
+                    }
+                    break;
+                }
+                case '(': {
+                    stackParentheses.push(new Node("1", 1));
+                    break;
+                }
+                case ')': {
+                    if (stackParentheses.empty()) {
+                        error = true;
+                    } else {
+                        stackParentheses.pop();
+                    }
+                    break;
+                }
+                case '{': {
+                    stackCurlyBrackets.push(new Node("1", 1));
+                    break;
+                }
+                case '}': {
+                    if (stackCurlyBrackets.empty()) {
+                        error = true;
+                    } else {
+                        stackCurlyBrackets.pop();
+                    }
+                    break;
+                }
+            }
+            if (error) {
                 break;
             }
+        }
 
-            //list.add(new Node(id + "", new Nothing(id, name, email)));
-            //list.insertPosN(1, new Node(id + "", new Nothing(id, name, email)));
-            //list.insert(new Node(id + "", new Integer(id)));
-            list.append(new Node(sid + "", new Integer(id)));
-        }
-        
-        ListaLinearDupla lld = list.sort(false);
-        
-        lld.show();
-        
-        //Object[] array = list.invArray();
-        
-        //for (int i=0; i < array.length; i++)
-        //{
-        //    System.out.println("[" + array[i] + "]" );
-        //}
-        
-        //while (!list.empty())
-        //{
-        //    Node aux = list.pop();
-         //   System.out.println("Node value: " + aux.getValue().toString());
-        //}
-        
-        //System.out.print("Pesquisar Id. Informe o Id: ");
-        //id = in.nextInt();
-        //Node nodeRet = list.search(id + "");
-        //String sret = nodeRet == null ? "*NULL*" : nodeRet.getId() + " - " + nodeRet.getValue().toString();
-        //System.out.println("\nRetorno -> " + sret);
-        /*
-        System.out.println("\n\n");
-        list.show();
-        System.out.println("\n\n");
-        list.insertPosN(3, new Node(id + "", new Nothing(id, name, email)));
-        System.out.println("\n\n");
-        list.show();
-        list.insertPosN(2, new Node(id + "", new Nothing(id, name, email)));
-        System.out.println("\n\n");
-        list.show();
-        System.out.println("\n\nRemovendo node 2\n\n");
-        list.removePosN(2);
-        list.show();
-        System.out.println("\n\nRemovendo nodew 3\n\n");
-        list.removePosN(3);
-        list.show();
-        /*
-        System.out.println("Pesquisar n-esimo. Informe a posicao: ");
-        int position = in.nextInt();
-        
-        Node searchedNodeClone = list.searchWithClone(position);
-        NodeValue searchedNodeInterface = list.searchWithInterface(position);
-        
-        System.out.println("Node: " + searchedNodeClone.getValue());
-        System.out.println("Node: " + searchedNodeInterface.getValue());
-         */
- /*
-        System.out.println("Removendo o primeiro no da lista: ");
-        Node nodeRet = list.pop();
-        if (nodeRet != null) {
-            System.out.println("No removido: " + nodeRet.getId() + " " + nodeRet.getValue());
-        }
-        list.show();
-        System.out.println("\n\n\n");
-
-        nodeRet = list.pop();
-        if (nodeRet != null) {
-            System.out.println("No removido: " + nodeRet.getId() + " " + nodeRet.getValue());
-        }
-        Node aux = list.searchContent("2");
-        if (aux != null) {
-            System.out.println("\nSearch content: 2 -> " + aux.getValue().toString());
+        if (error
+                || !stackSquareBrackets.empty()
+                || !stackParentheses.empty()
+                || !stackCurlyBrackets.empty()) {
+            System.out.println("\nExpressao incorreta");
         } else {
-            System.out.println("\nContent is NULL\n\n");
+            System.out.println("\nExpressão Ok");
         }
-         */
-        System.out.println("\n\n");
-        list.show();
     }
-
 }
